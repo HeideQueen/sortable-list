09b7ebb14565ddfaab3a7cfd7888ad1c9a96475e
HeideQueen <lucyfabz@gmail.com>
2020-07-10 22:54:16 -0500

    insert list items into DOM

---
+++ script.js
@@ -0,0 +1,43 @@
+const draggableList = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const vTubers = [
+  'Usada Pekora',
+  'Suzuhara Lulu',
+  'Ookami Mio',
+  'Inugami Korone',
+  'Nakiri Ayame',
+  'Nekomata Okayu',
+  'Houshou Marine',
+  'Amane Kanata',
+  'Tokoyami Towa',
+  'Akai Haato',
+];
+
+// store list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// insert list items into DOM
+function createList() {
+  [...vTubers].forEach((vTuber, index) => {
+    const listItem = document.createElement('li');
+
+    listItem.setAttribute('data-index', index);
+
+    listItem.innerHTML = `
+      <span class='number'>${index + 1}</span>
+      <div class='draggable' draggable='true'>
+        <p class='vtuber-name'>${vTuber}</p>
+        <i class='fa fa-grip-lines'></i>
+      </div>
+    `;
+
+    listItems.push(listItem);
+
+    draggableList.appendChild(listItem);
+  });
+}
